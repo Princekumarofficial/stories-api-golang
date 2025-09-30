85dbab9a932b59366d7f7ba4cf6353540f7e3222
Prince Kumar <x.prince.x@outlook.com>
2025-09-30 19:57:42 +0530

    basic http returns

--- cmd/stories-service/main.go
+++ cmd/stories-service/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/princekumarofficial/stories-service/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// setup server
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+	// setup router
+
+	server := http.Server{
+		Addr:    cfg.HTTPServer.Address,
+		Handler: router,
+	}
+
+	log.Println("server started on", cfg.HTTPServer.Address)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server: %s", err)
+	}
 }
